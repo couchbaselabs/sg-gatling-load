7bd947778dd06d30fe18bc748c5ff01fd0da20cf
Andrew Reslan <andrew@couchbase.com>
2015-04-01 19:58:06 +0100

    Modified to use custom simulation

---
+++ src/test/scala/computerdatabase/SGSimulation.scala
@@ -0,0 +1,47 @@
+package computerdatabase
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class SGimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://localhost:4985/db") // Here is the root for all relative URLs
+    .inferHtmlResources()
+    .acceptHeader("application/json")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-us")
+    .connection("keep-alive")
+    .contentTypeHeader("application/json")
+    .userAgentHeader("CouchbaseLite/1.0-Debug (iOS)")
+
+  
+
+    val writers = scenario("SG Writers").exec(Write.write)
+    val consumers = scenario("SG Consumers").exec(Consume.consume)
+
+    setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
+
+    setUp(
+      writers.inject(rampUsers(100) over (100 seconds)),
+      consumers.inject(rampUsers(1000) over (300 seconds))
+    ).protocols(httpConf)
+}
+
+object Write {
+
+  val post_headers = Map("Content-Type" -> "application/json")
+
+  val write = repeat(100, "n") {
+    exec(http("Create Doc ${n}")
+      .put("/doc${n}")
+      .headers(post_headers)
+      .body(RawFileBody("create_doc_request.txt"))))
+      .pause(1)
+  }
+}
+
+object Consume {
+  val consume = exec(http("Write Doc").get("/"))
+}
