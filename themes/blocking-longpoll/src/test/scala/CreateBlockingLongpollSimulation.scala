982d96a0b688d2b87970ceff64497b31dfab298c
Andrew Reslan <andrew@couchbase.com>
2015-06-24 13:10:24 +0100

    Updated Param names

--- themes/blocking-longpoll/src/test/scala/CreateBlockingLongpollSimulation.scala
+++ themes/blocking-longpoll/src/test/scala/CreateBlockingLongpollSimulation.scala
@@ -10,28 +10,28 @@ import scala.util.Random
 class CreateBlockingLongpollSimulation extends Simulation {
 
   //Get properties from jVM arguments
-  val targetHosts=java.lang.System.getProperty("targetHosts","localhost")
-  val database=java.lang.System.getProperty("database","sync_gateway")
-  val docSize=scala.Int.unbox(java.lang.Integer.getInteger("docSize",1024))
-  val rampUpIntervalMs=scala.Int.unbox(java.lang.Integer.getInteger("rampUpIntervalMs",3600000))
-  val runTimeMs=scala.Int.unbox(java.lang.Integer.getInteger("rampUpIntervalMs",7200000))
-  val sleepTimeMs=scala.Int.unbox(java.lang.Integer.getInteger("sleepTimeMs",10000))
-  val numPullers=scala.Int.unbox(java.lang.Integer.getInteger("numPullers",700))
-  val numPushers=scala.Int.unbox(java.lang.Integer.getInteger("numPushers",300))
-
-  val channelActiveUsers=scala.Int.unbox(java.lang.Integer.getInteger("channelActiveUsers",40))
-  val channelConcurrentUsers=java.lang.Integer.getInteger("channelConcurrentUsers",8)
-  val minUserOffTimeMs=scala.Int.unbox(java.lang.Integer.getInteger("minUserOffTimeMs",10000))
-  val maxUserOffTimeMs=scala.Int.unbox(java.lang.Integer.getInteger("minUserOffTimeMs",60000))
+  val testParamTargetHosts=java.lang.System.getProperty("testParamTargetHosts","localhost")
+  val testParamDatabase=java.lang.System.getProperty("testParamDatabase","sync_gateway")
+  val testParamDocSize=scala.Int.unbox(java.lang.Integer.getInteger("testParamDocSize",1024))
+  val testParamRampUpIntervalMs=scala.Int.unbox(java.lang.Integer.getInteger("testParamRampUpIntervalMs",3600000))
+  val testParamRunTimeMs=scala.Int.unbox(java.lang.Integer.getInteger("testParamRunTimeMs",7200000))
+  val testParamSleepTimeMs=scala.Int.unbox(java.lang.Integer.getInteger("testParamSleepTimeMs",10000))
+  val testParamNumPullers=scala.Int.unbox(java.lang.Integer.getInteger("testParamNumPullers",700))
+  val testParamNumPushers=scala.Int.unbox(java.lang.Integer.getInteger("testParamNumPushers",300))
+  val testParamChannelActiveUsers=scala.Int.unbox(java.lang.Integer.getInteger("testParamChannelActiveUsers",40))
+  val testParamChannelConcurrentUsers=java.lang.Integer.getInteger("testParamChannelConcurrentUsers",8)
+  val testParamMinUserOffTimeMs=scala.Int.unbox(java.lang.Integer.getInteger("testParamMinUserOffTimeMs",10000))
+  val testParamMaxUserOffTimeMs=scala.Int.unbox(java.lang.Integer.getInteger("testParamMaxUserOffTimeMs",60000))
 
   //HTTP parameters
-  val restApiProtocol=java.lang.System.getProperty("restApiProtocol","http") //http or https
-  val adminRestApiPort=scala.Int.unbox(java.lang.Integer.getInteger("adminRestApiPort",4985))
-  val userRestApiPort=scala.Int.unbox(java.lang.Integer.getInteger("userRestApiPort",4984))
+  val httpParamRestApiProtocol=java.lang.System.getProperty("httpParamRestApiProtocol","http") //http or https
+  val httpParamAdminRestApiPort=scala.Int.unbox(java.lang.Integer.getInteger("httpParamAdminRestApiPort",4985))
+  val httpParamUserRestApiPort=scala.Int.unbox(java.lang.Integer.getInteger("httpParamUserRestApiPort",4984))
 
 
   //REST API request Parameters
   val queryParamFeed=java.lang.System.getProperty("queryParamFeed","continuous")
+  //TODO: since can be any JSON value
   val queryParamSince=scala.Int.unbox(java.lang.Integer.getInteger("queryParamSince",0))
   val querytParamHeartbeat=scala.Int.unbox(java.lang.Integer.getInteger("queryParamHeartbeat",60000))
   val querytParamConflicts=java.lang.Boolean.getBoolean("querytParamConflicts")
@@ -40,11 +40,38 @@ class CreateBlockingLongpollSimulation extends Simulation {
   val queryParamEnd_Key=java.lang.System.getProperty("queryParamEnd_key","")
   val queryParamEndkey_docid=java.lang.System.getProperty("queryParamEndkey_docid","")
   val queryParamEnd_Key_doc_id=java.lang.System.getProperty("queryParamEnd_Key_doc_id","")
-  val querytParamInclude_docs=java.lang.Boolean.getBoolean("querytParamInclude_docs")
-  val querytParamInclusive_end=java.lang.Boolean.getBoolean("querytParamInclusive_end")
+  val queryParamInclude_docs=java.lang.Boolean.getBoolean("queryParamInclude_docs")
+  val queryParamInclusive_end=java.lang.Boolean.getBoolean("queryParamInclusive_end")
+  val queryParamKey=java.lang.System.getProperty("queryParamKey","")
+  val querytParamLimit=scala.Int.unbox(java.lang.Integer.getInteger("querytParamLimit",0))
+  val querytParamSkip=scala.Int.unbox(java.lang.Integer.getInteger("querytParamSkip",0))
+  val queryParamStale=java.lang.System.getProperty("queryParamStale","ok")
+  val queryParamStartkey=java.lang.System.getProperty("queryParamStartkey","")
+  val queryParamStart_key=java.lang.System.getProperty("queryParamStart_key","")
+  val queryParamStartkey_docid=java.lang.System.getProperty("queryParamStartkey_docid","")
+  val queryParamStart_key_doc_id=java.lang.System.getProperty("queryParamStart_key_doc_id","")
+  val querytParamUpdate_seq=java.lang.Boolean.getBoolean("querytParamUpdate_seq")
+  val queryParamAttachments=java.lang.Boolean.getBoolean("queryParamAttachments")
+  val queryParamAtt_encoding_info=java.lang.Boolean.getBoolean("queryParamAtt_encoding_info")
+  val queryParamDeleted_conflicts=java.lang.Boolean.getBoolean("queryParamDeleted_conflicts")
+  //atts_since
+  val queryParamLatest=java.lang.Boolean.getBoolean("queryParamLatest")
+  val queryParamLocal_seq=java.lang.Boolean.getBoolean("queryParamLocal_seq")
+  val queryParamMeta=java.lang.Boolean.getBoolean("queryParamMeta")
+  //open_revs
+  val queryParamRev=java.lang.System.getProperty("queryParamRev","")
+  val queryParamRevs=java.lang.Boolean.getBoolean("queryParamRevs")
+  val queryParamRevs_info=java.lang.Boolean.getBoolean("queryParamRevs_info")
+//doc_ids
+  val queryParamFilter=java.lang.System.getProperty("queryParamFilter","")
+  val querytParamLast_event_id=scala.Int.unbox(java.lang.Integer.getInteger("querytParamLast_event_id",0))
+  val queryParamStyle=java.lang.System.getProperty("queryParamStyle","")
+  val querytParamTimeout=scala.Int.unbox(java.lang.Integer.getInteger("querytParamTimeout",0))
+  val queryParamView=java.lang.System.getProperty("queryParamView","")
+
 
   //Generate a list of target URL's from the list of target hosts
-  val targetURLs = targetHosts.split(",").map(_.trim.replaceFirst("^", restApiProtocol+"://").concat(":"+userRestApiPort+"/"+database)).toList
+  val targetURLs = targetHosts.split(",").map(_.trim.replaceFirst("^", httpParamRestApiProtocol+"://").concat(":"+httpParamUserRestApiPort+"/"+database)).toList
 
   System.err.println("targetURL's = "+targetURLs)
 
